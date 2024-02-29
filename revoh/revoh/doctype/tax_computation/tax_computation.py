ff875bdd3b31b17e9c0ccdb8b95a4cd63e827086
janishatech <janisha1508@gmail.com>
2024-02-29 18:41:37 +0530

    feature :tax computation tax calculation done for new regim

--- revoh/revoh/doctype/tax_computation/tax_computation.py
+++ revoh/revoh/doctype/tax_computation/tax_computation.py
@@ -9,17 +9,35 @@ class TaxComputation(Document):
 
 @frappe.whitelist()
 def get_gross_pay(employee, start_date, end_date):
-	start_date = frappe.utils.data.getdate(start_date)
-	end_date = frappe.utils.data.getdate(end_date)
-	gross_pay_sum = 0
-	current_date = start_date
-	while current_date <= end_date:
-		first_day = frappe.utils.data.get_first_day(current_date)
-		last_day = frappe.utils.data.get_last_day(current_date)
-		sql_query = """SELECT SUM(gross_pay) as total_gross_pay FROM `tabSalary Slip` WHERE employee = %s AND start_date >= %s AND end_date <= %s"""
-		result = frappe.db.sql(sql_query, (employee, first_day, last_day), as_dict=True)
-		if result and result[0].total_gross_pay:
-			gross_pay_sum += result[0].total_gross_pay
-		current_date = frappe.utils.data.add_months(current_date, 1)
-		
-	return gross_pay_sum
+    start_date = frappe.utils.data.getdate(start_date)
+    end_date = frappe.utils.data.getdate(end_date)
+    gross_pay_sum = 0
+    
+    # Fetch salary slips for the specified employee and date range
+    salary_slips = frappe.get_list(
+        "Salary Slip",
+        filters={
+            "employee": employee,
+            "start_date": (">=", start_date),
+            "end_date": ("<=", end_date)
+        },
+        fields=["name"]
+    )
+    
+    for slip in salary_slips:
+        # Fetch earnings for each salary slip where component is "Gross Pay"
+        earnings = frappe.get_list(
+            "Salary Detail",
+            filters={
+                "parent": slip.name,
+                "parenttype": "Salary Slip",
+                "parentfield": "earnings",
+                "salary_component": "Gross Pay" 
+            },
+            fields=["amount"]
+        )
+        
+        if earnings:
+            gross_pay_sum += earnings[0].amount
+    
+    return gross_pay_sum
