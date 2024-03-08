142ebe23ce99b27b37b236e0a4651234ee84ae3b
janishatech <janisha1508@gmail.com>
2024-03-08 12:45:30 +0530

    feature:salary slip email trigger for after submit

--- revoh/hooks.py
+++ revoh/hooks.py
@@ -120,13 +120,16 @@ override_doctype_class = {
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-#	"*": {
-#		"on_update": "method",
-#		"on_cancel": "method",
-#		"on_trash": "method"
-#	}
-# }
+doc_events = {
+    "Salary Slip": {
+		"on_submit": "revoh.custom.send_salary_slip_email",
+	},
+	# "*": {
+	# 	"on_update": "method",
+	# 	"on_cancel": "method",
+	# 	"on_trash": "method"
+	# }
+}
 
 # Scheduled Tasks
 # ---------------
